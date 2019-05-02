fde47b01cb418e15fd8ffd1db1d48498496590b9
Daniel Stamer <dan@hello-world.sh>
2019-05-02 23:11:24 +0200

    first version of urly-wurly
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- Makefile
+++ Makefile
@@ -1,12 +1,20 @@
-GCP_ID := $(shell gcloud config get-value project)
-IMAGE := "gcr.io/${GCP_ID}/run-service"
+PROJECT := $(shell gcloud config get-value project)
+IMAGE := gcr.io/${PROJECT}/urly-wurly
+BUCKET := urly-wurly-links
+DOMAIN := urly-wurly-p53dlhmcna-uc.a.run.app
 
-package:
-	gcloud auth configure-docker
-	docker build container/ -t ${IMAGE}
-	docker push ${IMAGE}
+storage:
+	gsutil mb gs://${BUCKET}
+
+build:
+	gcloud builds submit container/ --tag ${IMAGE}
 
 deploy:
 	gcloud config set run/region us-central1
-	gcloud beta run deploy run-service --allow-unauthenticated --image ${IMAGE} 
+	gcloud beta run deploy urly-wurly \
+		--allow-unauthenticated \
+	  --image ${IMAGE} \
+		--set-env-vars=DOMAIN=${DOMAIN},PROJECT=${PROJECT},BUCKET=${BUCKET}
 
+meta-data:
+	gcloud beta run services describe urly-wurly
--- container/server.rb
+++ container/server.rb
@@ -1,8 +1,36 @@
+require 'adler32'
+require 'json'
+require 'google/cloud/storage'
 require 'sinatra'
 
 set :bind, '0.0.0.0'
 set :port, ENV["PORT"]
 
-get '/' do
-  "I am running on Cloud Run!"
+get '/' do 
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/s' do
+  return "no url to shorten supplied!" unless params['url']
+  domain = ENV['DOMAIN']
+  full_url = params['url'].gsub(/\A"|"\Z/, '').gsub(/\A'|'\Z/, '')
+  hash = Adler32.checksum full_url
+  storage = Google::Cloud::Storage.new project_id: ENV['PROJECT']
+  bucket = storage.bucket 'urly-wurly-links'
+  bucket.create_file StringIO.new(full_url), hash
+  {
+    shortened_url: "https://#{domain}/l/#{hash}"
+  }.to_json 
+end
+
+get '/l/:hash' do
+  hash = params[:hash]
+  storage = Google::Cloud::Storage.new project_id: ENV['PROJECT']
+  bucket = storage.bucket 'urly-wurly-links'
+  file = bucket.file hash
+  return "link not found!" unless file
+  content = file.download
+  content.rewind
+  status 301
+  response['Location'] = content.read
 end
