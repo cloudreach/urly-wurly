8711a1cf08d7f30aa1a7b05c31ce2ac1492beecf
Daniel Stamer <dan@hello-world.sh>
2019-05-03 23:08:38 +0200

    add docs and refactor
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- container/server.rb
+++ container/server.rb
@@ -9,77 +9,97 @@ set :bind, '0.0.0.0'
 set :port, ENV["PORT"]
 
 def short_code(url)
+  # Create short code key as substitute URL
   return Base64.encode64(Digest::SHA1.hexdigest(url))[22..27]
 end
 
 def gcs_write(key, content)
+  # Write string content to GCS object identified by key
   storage = Google::Cloud::Storage.new project_id: ENV['PROJECT']
   bucket = storage.bucket 'urly-wurly-links'
   bucket.create_file StringIO.new(content), key
 end
 
 def gcs_read(key)
+  # Read object from GCS, identified by key
   storage = Google::Cloud::Storage.new project_id: ENV['PROJECT']
   bucket = storage.bucket 'urly-wurly-links'
   return bucket.file key
 end
 
 get '/' do 
+  # Root path, serving out static SPA
   File.read(File.join('public', 'index.html'))
 end
 
 get '/s' do
+  # Endpoint to shorten a longer URL
   return {
       message: 'no url to shorten provided!',
   }.to_json unless params['url']
 
+  # Trim ' and " chars from URL parameter
   full_url = params['url'].gsub(/\A"|"\Z/, '').gsub(/\A'|'\Z/, '')
-  full_uri = URI.parse(full_url)
-
+  
+  # Try parsing out required URL schemes
+  full_uri = URI.parse full_url
   return {
       message: 'provided input is not a HTTP/HTTPS URL!'
   }.to_json unless ['https', 'http'].include? full_uri.scheme
 
-  hash = short_code(full_url)
+  # Compute short code and persist
+  shortcode = short_code full_url
   gcs_write(hash, full_url)
 
+  # Construct new URL and respond
   domain = ENV['DOMAIN']
-
+  response['Content-Type'] = 'application/json'
   return {
-    shortened_url: "https://#{domain}/l/#{hash}",
+    shortened_url: "https://#{domain}/l/#{shortcode}",
     message: "url shortened!"
   }.to_json
 end
 
-get '/l/:hash' do
-  file = gcs_read(params['hash'])
+get '/l/:shortcode' do
+  # Endpoint to reverse shortening
+  file = gcs_read(params['shortcode'])
 
+  # Unable to find persisted long URL for given hash
   return {
-      message: 'unable to find link!'
+      message: 'unable to find URL!'
   } unless file
 
+  # Read object and reset scanner index
   content = file.download 
   content.rewind
+
+  # Set HTTP redirect
   status 301
   response['Location'] = content.read
 end
 
-post '/slack' do 
-  full_url = params['text'].strip
-  full_uri = URI.parse(url)
-
+post '/slack' do
+  # Shorten URL via Slack
   return {
-    text: 'Error: provided input is not a HTTP/HTTPS URL!'
+    message: 'no url to shorten provided!',
+  }.to_json unless params['text']
+
+  full_url = params['text'].strip
+
+  # Try parsing out required URL schemes
+  full_uri = URI.parse(url)
+  return {
+    text: 'provided input is not a HTTP/HTTPS URL!'
   }.to_json unless ['https', 'http'].include? full_uri.scheme
 
-  hash = short_code full_url
-  gcs_write(hash, full_url)
+  # Compute short code and persist
+  shortcode = short_code full_url
+  gcs_write(shortcode, full_url)
 
+  # Construct new URL and respond
   domain = ENV['DOMAIN']
   response['Content-Type'] = 'application/json'
-
   return {
-    text: "Shortened URL: https://#{domain}/l/#{hash}",
+    text: "Shortened URL: https://#{domain}/l/#{shortcode}",
   }.to_json
 end
-
