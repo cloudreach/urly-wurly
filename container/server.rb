75a8019e97ad05646e6176674cbfac10698c7746
Daniel Stamer <dan@hello-world.sh>
2019-05-07 18:26:48 +0200

    add stackdriver custom metrics
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- container/server.rb
+++ container/server.rb
@@ -1,6 +1,7 @@
 require 'base64'
 require 'digest/sha1'
 require 'google/cloud/storage'
+require 'google/cloud/monitoring'
 require 'json'
 require 'sinatra'
 require 'uri'
@@ -11,6 +12,44 @@ set :port, ENV['PORT']
 def short_code(url)
   # Create short code key as substitute URL
   Base64.encode64(Digest::SHA1.hexdigest(url))[22..27]
+end
+
+def stackdriver_create(name, description)
+  # Create Stackdriver metric descriptor
+  client = Google::Cloud::Monitoring::Metric.new
+  project_name = Google::Cloud::Monitoring::V3::MetricServiceClient.project_path ENV['PROJECT']
+
+  descriptor = Google::Api::MetricDescriptor.new(
+    type:        "custom.googleapis.com/#{name}",
+    metric_kind: Google::Api::MetricDescriptor::MetricKind::CUMULATIVE,
+    value_type:  Google::Api::MetricDescriptor::ValueType::INT64,
+    description: description
+  )
+
+  result = client.create_metric_descriptor  project_name, descriptor
+end
+
+def stackdriver_increment(name)
+  # Publish metric data to Stackdriver
+  client = Google::Cloud::Monitoring::Metric.new
+  project_name = Google::Cloud::Monitoring::V3::MetricServiceClient.project_path ENV['PROJECT']
+
+  series = Google::Monitoring::V3::TimeSeries.new
+  metric = Google::Api::Metric.new type: "custom.googleapis.com/#{name}"
+  series.metric = metric
+
+  resource = Google::Api::MonitoredResource.new type: "run_service"
+  resource.labels["service_name"] = "urly-wurly"
+  series.resource = resource
+
+  point = Google::Monitoring::V3::Point.new
+  point.value = Google::Monitoring::V3::TypedValue.new int64_value: 1
+  now = Time.now
+  end_time = Google::Protobuf::Timestamp.new seconds: now.to_i, nanos: now.usec
+  point.interval = Google::Monitoring::V3::TimeInterval.new end_time: end_time
+  series.points << point
+
+  client.create_time_series project_name, [series]
 end
 
 def gcs_write(key, content)
@@ -33,6 +72,11 @@ get '/' do
 end
 
 get '/s' do
+  begin 
+    stackdriver_create('shortening-invocation-web', 'some BS i made up')
+  end
+  stackdriver_increment('shortening-invocation-web')
+
   # Endpoint to shorten a longer URL
   return {
     message: 'no url to shorten provided!'
